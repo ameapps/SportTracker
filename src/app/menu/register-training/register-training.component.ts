cd55dace285116af23639d2b62d5322c2c930c81
Stefano <git.stefano@outlook.it>
2021-10-10 00:28:10 +0200

    select material aggiunta anche se trasparente

--- src/app/menu/register-training/register-training.component.ts
+++ src/app/menu/register-training/register-training.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-register-training',
@@ -6,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-training.component.scss'],
 })
 export class RegisterTrainingComponent implements OnInit {
+  toppings = new FormControl();
+
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
 
   constructor() { 
   }
--- src/app/menu/register-training/register-training.module.ts
+++ src/app/menu/register-training/register-training.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { RegisterTrainingRoutingModule } from './register-training-routing.module';
 import { RegisterTrainingComponent } from './register-training.component';
+import {MatSelectModule} from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DemoMaterialModule } from 'src/modules/material/material.module';
 
 
 @NgModule({
   declarations: [RegisterTrainingComponent],
   imports: [
     CommonModule,
-    RegisterTrainingRoutingModule
+    RegisterTrainingRoutingModule,
+    ReactiveFormsModule,
+    MatSelectModule
   ]
 })
 export class RegisterTrainingModule { }
