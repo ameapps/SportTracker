fe91662141db217b877582f109a2f5eb976984a4
Stefano <git.stefano@outlook.it>
2021-10-20 23:52:53 +0200

    Al click su uno strumento cambia il colore dell'icona e dello sfondo

--- src/app/common/selector/selector.component.ts
+++ src/app/common/selector/selector.component.ts
@@ -7,22 +7,53 @@ import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/cor
 })
 export class SelectorComponent implements OnInit, OnChanges {
 
-  arr = [1,2,3,4,5,6,7,8,8,8,8,8,8,8,8,8,8];
+  
 
   @Input() selections: object[];
+  clicked: boolean[] = [];
 
   constructor() { }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes["selections"]) {
       this.selections = changes["selections"].currentValue;
+      this.clicked = this.initClicked();
       console.log(this.selections)
     }
   }
 
-  ngOnInit() {
+  /**
+   * Method setting the clicked array, creating
+   * a boolean array representing the click state
+   * of each strument. By default, they will be set
+   * as unclicked.
+   */
+  initClicked(): boolean[] {
+    this.selections.forEach(strument => {
+      this.clicked.push(false);
+    });
+    return this.clicked;
+  }
 
-    console.log(`Struments: ${this.selections}`)
+  ngOnInit() {}
+
+  /**
+   * Method setting as true/false the clicked
+   * image boolean state in clicked array.
+   * @param index 
+   */
+  strumentSelected(index: number) {
+    this.clicked[index] = !this.clicked[index];
+  }
+
+  /**
+   * Click event manager indicating whether 
+   * the strument is clicked or unclicked.
+   * @param index of the struments
+   * @returns true or false depending on its click state
+   */
+  isClicked(index: number) {
+    return this.clicked[index];
   }
 
 }
