796e132d861ca43275e3daa77e33b6b92b0d076b
Stefano <git.stefano@outlook.it>
2021-10-31 18:24:02 +0100

    metto chrono time picker e clock nel componente timer

--- src/app/common/timer/timer.component.ts
+++ src/app/common/timer/timer.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { CustomTrainingService } from 'src/services/App/Custom training/custom-training.service';
+import { threadId } from 'worker_threads';
 
 @Component({
   selector: 'app-timer',
@@ -6,15 +8,19 @@ import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/cor
   styleUrls: ['./timer.component.scss'],
 })
 export class TimerComponent implements OnInit, OnChanges {
-
-  hours = 0;
-  minutes = 0;
-  seconds = 0; 
-  tenths = 0;
-
-  @Input() leftTime: string;
    
-  constructor() { 
+  // CHRONO TIME PICKER
+  @Input() chronoType: string = "";
+  @Output() definedTime = new EventEmitter();
+  @Output() actualTime = new EventEmitter<string>();
+
+  // DIGITAL CLICK COMPONENT
+  @Input() hours: number | string;
+  @Input() minutes: number | string;
+  @Input() seconds: number | string;
+
+
+  constructor(private customTrainingService: CustomTrainingService) { 
 
   }
 
@@ -25,6 +31,16 @@ export class TimerComponent implements OnInit, OnChanges {
   ngOnInit() {
   }
 
+  //#region methods
+  onDefinedTime(event){
+    this.definedTime.emit(event);
+  }
 
+  onActualTime(event) {
+    this.actualTime.emit(event);
+  }
+
+
+  //#endregion
 
 }
