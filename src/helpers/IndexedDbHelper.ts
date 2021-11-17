eaa57461bd2e1045b1ab5c445acb579d73643347
Stefano <git.stefano@outlook.it>
2021-11-17 21:27:00 +0100

    si vedono le immagini anche sulla versione pubblicata (non su mobile però)

--- src/helpers/IndexedDbHelper.ts
+++ src/helpers/IndexedDbHelper.ts
@@ -28,6 +28,70 @@ export class IndexedDbHelper {
         return myPromise;
     }
 
+    /**Method getting the specified objectstore from the specified database. 
+     * @indexedDb database from which getting the specified objectstore 
+     * @objectName name of the objectstore to get from the database.
+    */
+    static GetDbObjectstore(indexesDb, objectName) {
+        const transaction = indexesDb.transaction([objectName]);
+        const objectStore = transaction.objectStore(objectName);
+        return objectStore;
+    }
+
+    /**Method allowing to get an element from an objectStore object
+     * using the specified primary key. 
+     * This methos incapsulates an async callback call when 
+     * onsuccess event is triggered into a promise. 
+     * This allows to resolve the promise in OOP style.
+     * @objectStore database object from which getting data
+     * @elementPrimaryKey string representing the primary key of the element to get from the object.
+     */
+    static async getObjectElement(objectStore, elementPrimaryKey) {
+        var objectStore2 = objectStore.get(elementPrimaryKey);
+        let element = null;
+        const prom = new Promise((resolve, reject) => {
+            objectStore2.onsuccess = function(event) {
+                if(objectStore2.result) {
+                  console.log('success message!');
+                  element = objectStore2.result;
+                  resolve(element);
+                } else {
+                  console.log('error message!');  
+                  element = null;
+                  resolve(element);
+                }
+             };
+        });
+
+        return prom;
+    }
+
+    /**
+     * Method getting all the object from the specified objectstore 
+     * available from the specified object name.
+     * @param indexesDb database object 
+     * @param objectName name of the objetstore included into the database to use
+     * @returns an array of gotten elements
+     */
+    static async AllObjectstoreElements(indexesDb, objectName) {
+        const values = [];
+        const objectStore = IndexedDbHelper.GetDbObjectstore(indexesDb, objectName);
+        
+        const prom = new Promise((resolve, reject) => {
+          objectStore.openCursor().onsuccess = function(event) {
+            var cursor = event.target.result;
+            if (cursor) {
+              values.push(cursor.value);
+               cursor.continue();
+            } else {
+              resolve(values);
+            }
+         };
+        });
+    
+        return prom;
+     }
+
     /**
      * Method getting the geys from the given database
      * @param database IDBDatabase object. It has the same format: 
@@ -48,5 +112,13 @@ export class IndexedDbHelper {
 
     static getTable(idTable) {
 
+    }
+
+    /**Method showing an example of how to use this class. */
+    static async usageExample() {
+        const indexesDb = await IndexedDbHelper.openDb('Disc') as any;
+        var objectStore = IndexedDbHelper.GetDbObjectstore(indexesDb, 'FileStorage');
+        const dbElement = await IndexedDbHelper.getObjectElement(objectStore, '/DATA/1636315998653.jpeg');
+        const pictures = await IndexedDbHelper.AllObjectstoreElements(indexesDb, ['FileStorage']);
     }
 }
--- src/services/App/Database/Ionic storage/ionic-storage.service.ts
+++ src/services/App/Database/Ionic storage/ionic-storage.service.ts
@@ -6,6 +6,7 @@ import { IDatabase } from 'src/services/Interfaces/Database';
 
 import { DomSanitizer } from '@angular/platform-browser';
 import { IndexedDbHelper } from 'src/helpers/IndexedDbHelper';
+import { Router } from '@angular/router';
 
 
 @Injectable({
@@ -13,7 +14,10 @@ import { IndexedDbHelper } from 'src/helpers/IndexedDbHelper';
 })
 export class IonicStorageService implements IDatabase {
 
-  constructor(private storage: StorageService, private sanitizer:DomSanitizer) { }
+  constructor(private storage: StorageService, 
+    private sanitizer: DomSanitizer,
+    private router: Router
+    ) { }
 
   /**Prototype to get all elements associated to the specified entity */
   async GetAllItems(datatype: DbDataType): Promise<object[]> {
@@ -21,29 +25,21 @@ export class IonicStorageService implements IDatabase {
     switch (datatype) {
       case DbDataType.GALLERY:
         console.log('fire GetAllItems')
-        const key: string = DbEntities[DbEntities.PHOTO_STORAGE];
-        const el = await this.storage.get(key);
-        let arr = JSON.parse(el.value);
+        
+        let allPhotos = await this.getPhotoes();
+
+        let arr: any = allPhotos;
         items = arr;
 
-        /* VERSIONE PROMISE */
         let gottenkeys = [];
-        const indexesDb = await IndexedDbHelper.openDb(key) as any;
-        let saved = null;
-        const keys = await IndexedDbHelper.getDbKeys(indexesDb);
-        var trans = indexesDb.transaction(['folder'], "readwrite");
-        var store = trans.objectStore(key);
-        console.log('indexDB test');
-
-        for (let index = 0; index < arr.length; index++) {
-          let element = arr[index];
-
+        for (let index = 0; index < allPhotos.length; index++) {
+          let element = allPhotos[index];
           const blob = this.convertBase64ToBlob(element.blobBase64 as string);
-          let objectURLa = URL.createObjectURL(blob); 
+          let objectURLa = URL.createObjectURL(blob);
           const imageUrl = this.sanitizer.bypassSecurityTrustUrl(objectURLa);
-          arr[index].webviewPath = imageUrl;
-
+          allPhotos[index].webviewPath = imageUrl;
         }
+
 
         break;
       default:
@@ -52,14 +48,83 @@ export class IonicStorageService implements IDatabase {
     return items;
   }
 
-  dataURLToBlob(dataurl) {    
+
+  private async getPhotoes() {
+    let photoes = null;
+    if (this.isRunningOnlocalhost()) {
+      photoes = await this.imagesFromStorage();
+    }
+    else {
+      photoes = await this.IndexedDbAllPhotoes(); 
+      // MODIFICARE I CAMPI PER UNIFORMARLI A QUELLI DI STORAGE.
+      // content -> blobBase64
+      const keys = Object.keys(photoes[0]);
+      var formatted = this.asFromStorae(photoes, keys);
+      photoes = formatted;
+    }
+    return photoes;
+  }
+
+  /**Method chainging the specified object fielts renaming them
+   * making the object similiar to a storage object. */
+  private asFromStorae(photoes: any, keys: string[]) {
+    const buildedObjs: object[] = [];
+    let builded = new Object(); 
+    for (let index = 0; index < photoes.length; index++) {
+      const photo = photoes[index];
+      keys.forEach(key => {
+        switch (key) {
+          case 'content':
+            builded['blobBase64'] = `data:image/png;base64,${photo[key]}`;
+            break;
+          default:
+            builded[key] = photo[key];
+            break;
+        }
+      });
+      buildedObjs.push(builded);
+      builded = new Object(); 
+    }
+
+
+    return buildedObjs;
+  }
+
+  private async imagesFromStorage() {
+    const key: string = DbEntities[DbEntities.PHOTO_STORAGE];
+    const el = await this.storage.get(key);
+    let arr = JSON.parse(el.value);
+    return arr;
+  }
+
+  private isRunningOnlocalhost() {
+    let isLocalhost = false;
+    if (this.getUrl().includes('localhost')) {
+      isLocalhost = true;
+    }
+    return isLocalhost;
+  }
+
+  private getUrl(): string {
+    return window.location.href;
+  }
+
+  /**Method getting all the pictures from the indexedDb 
+   * when ionic storage can't get them. */
+  private async IndexedDbAllPhotoes() {
+    const indexesDb = await IndexedDbHelper.openDb('Disc') as any;
+    const pictures = await IndexedDbHelper.AllObjectstoreElements(indexesDb, ['FileStorage']);
+    return pictures;
+  }
+
+  dataURLToBlob(dataurl) {
     let arr = dataurl.split(',');
     let mime = arr[0].match(/:(.*?);/)[1];
     let bstr = atob(arr[1]);
     let n = bstr.length;
     let u8arr = new Uint8Array(n);
     while (n--) {
-        u8arr[n] = bstr.charCodeAt(n);
+      u8arr[n] = bstr.charCodeAt(n);
     }
     return new Blob([u8arr], { type: mime });
   }
@@ -68,25 +133,25 @@ export class IonicStorageService implements IDatabase {
  * Convert BASE64 to BLOB
  * @param base64Image Pass Base64 image data to convert into the BLOB
  */
-private convertBase64ToBlob(base64Image: string) {
-  // Split into two parts
-  const parts = base64Image.split(';base64,');
+  private convertBase64ToBlob(base64Image: string) {
+    // Split into two parts
+    const parts = base64Image.split(';base64,');
 
-  // Hold the content type
-  const imageType = parts[0].split(':')[1];
+    // Hold the content type
+    const imageType = parts[0].split(':')[1];
 
-  // Decode Base64 string
-  const decodedData = window.atob(parts[1]);
+    // Decode Base64 string
+    const decodedData = window.atob(parts[1]);
 
-  // Create UNIT8ARRAY of size same as row data length
-  const uInt8Array = new Uint8Array(decodedData.length);
+    // Create UNIT8ARRAY of size same as row data length
+    const uInt8Array = new Uint8Array(decodedData.length);
 
-  // Insert all character code into uInt8Array
-  for (let i = 0; i < decodedData.length; ++i) {
-    uInt8Array[i] = decodedData.charCodeAt(i);
+    // Insert all character code into uInt8Array
+    for (let i = 0; i < decodedData.length; ++i) {
+      uInt8Array[i] = decodedData.charCodeAt(i);
+    }
+
+    // Return BLOB image after conversion
+    return new Blob([uInt8Array], { type: imageType });
   }
-
-  // Return BLOB image after conversion
-  return new Blob([uInt8Array], { type: imageType });
-}
 }
