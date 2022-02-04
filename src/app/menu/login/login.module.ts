d1e9bada0fa50e8c2eea3239e4f78fc223d492eb
Stefano Amenta <git.stefano@outlook.it>
2022-02-04 23:53:47 +0100

    provato ad implementare l'autenticazione oAuth di google non funziona per qualche motivo. Il provider di google non funziona. Ho copiato il componente login dal progetto in  https://github.com/dnlrbz/social-login

---
+++ src/app/menu/login/login.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login.component';
+import { GoogleLoginProvider } from 'angularx-social-login';
+import { AuthGuardService } from './auth-guard.service';
+
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    LoginRoutingModule
+  ],
+  exports: [LoginComponent],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: true,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider('148517665605-jspahbqleats6lvlag9kasc2c11b5g7o.apps.googleusercontent.com')
+        }
+      ]
+    }
+  },
+    AuthGuardService
+  ],
+})
+export class LoginModule { }
--- src/app/menu/register-training/stepper/step 3/register-step3/rt-step3/rt-step3.module.ts
+++ src/app/menu/register-training/stepper/step 3/register-step3/rt-step3/rt-step3.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { RtStep3RoutingModule } from './rt-step3-routing.module';
 import { RtStep3Component } from './rt-step3.component';
+import { LoginComponent } from 'src/app/menu/login/login.component';
+import { GoogleLoginProvider } from 'angularx-social-login';
+import { AuthGuardService } from 'src/app/menu/login/auth-guard.service';
+import { LoginModule } from 'src/app/menu/login/login.module';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
@@ -10,7 +15,23 @@ import { RtStep3Component } from './rt-step3.component';
   exports: [RtStep3Component],
   imports: [
     CommonModule,
-    RtStep3RoutingModule
-  ]
+    RtStep3RoutingModule,
+    LoginModule,
+    MatCardModule
+  ],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: true,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider('148517665605-jspahbqleats6lvlag9kasc2c11b5g7o.apps.googleusercontent.com')
+        }
+      ]
+    }
+  },
+    AuthGuardService],
+  
 })
 export class RtStep3Module { }
