801d882eef0e6c73fd8406d4d81224f917bf9780
Stefano Amenta <git.stefano@outlook.it>
2022-02-05 18:41:19 +0100

    FIXED BUG GOOGLE OAUTH. non funziona però il client id. Errore 400

--- src/app/menu/login/login.module.ts
+++ src/app/menu/login/login.module.ts
@@ -5,6 +5,11 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { GoogleLoginProvider, SocialLoginModule } from 'angularx-social-login';
 import { AuthGuardService } from './auth-guard.service';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
@@ -12,6 +17,11 @@ import { AuthGuardService } from './auth-guard.service';
   imports: [
     CommonModule,
     LoginRoutingModule,
+    /**Needed modules for the login component */
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
     SocialLoginModule
   ],
   exports: [LoginComponent],
@@ -22,7 +32,7 @@ import { AuthGuardService } from './auth-guard.service';
       providers: [
         {
           id: GoogleLoginProvider.PROVIDER_ID,
-          provider: new GoogleLoginProvider('148517665605-jspahbqleats6lvlag9kasc2c11b5g7o.apps.googleusercontent.com')
+          provider: new GoogleLoginProvider('899314583596-npnuh1nsukq1t23ao4b0u49d1589tgbf.apps.googleusercontent.com')
         }
       ]
     }
