c0aedb8a8c3f5a0e5b723acf90c1ea09b31bd172
Stefano Amenta <git.stefano@outlook.it>
2025-07-29 22:00:24 +0200

    implementazione inserimento cibi mangiati

--- src/app/menu/food-history/food-history.component.ts
+++ src/app/menu/food-history/food-history.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonicModule } from "@ionic/angular";
 
 @Component({
   selector: 'app-food-history',
   templateUrl: './food-history.component.html',
-  styleUrls: ['./food-history.component.scss'],
+  styleUrls: ['./food-history.component.scss']
 })
-export class FoodHistoryComponent implements OnInit {
+export class FoodHistoryComponent {
+  foodForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.foodForm = this.fb.group({
+      name: ['', Validators.required],
+      quantity: [null, [Validators.required, Validators.min(0)]],
+      unit: ['', Validators.required],
+      category: ['', Validators.required],
+      calories: [null, [Validators.required, Validators.min(0)]],
+      carbs: [null, [Validators.min(0)]],
+      proteins: [null, [Validators.min(0)]],
+      fats: [null, [Validators.min(0)]],
+      fiber: [null, [Validators.min(0)]],
+      sugars: [null, [Validators.min(0)]],
+      dateTime: [new Date().toISOString(), Validators.required]
+    });
+  }
 
-  ngOnInit() {}
-
+  onSubmit() {
+    if (this.foodForm.valid) {
+      // Salva i dati o invia al servizio
+      console.log(this.foodForm.value);
+      // Reset o navigazione
+    }
+  }
 }
--- src/app/menu/food-history/food-history.module.ts
+++ src/app/menu/food-history/food-history.module.ts
@@ -3,9 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { FoodHistoryRoutingModule } from './food-history-routing.module';
 import { IonicModule } from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FoodHistoryComponent } from './food-history.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, FoodHistoryRoutingModule, IonicModule],
+  declarations: [FoodHistoryComponent], 
+  imports: [
+    CommonModule,
+    FoodHistoryRoutingModule,
+    IonicModule,
+    ReactiveFormsModule,
+  ],
 })
 export class FoodHistoryModule {}
