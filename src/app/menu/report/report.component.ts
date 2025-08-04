2681ac1293fa62fbbb04b7ebde46f62678db016a
Stefano Amenta <git.stefano@outlook.it>
2025-08-04 21:17:51 +0200

    Report: aggiunta paginazione ai grafici

--- src/app/menu/report/report.component.ts
+++ src/app/menu/report/report.component.ts
@@ -9,7 +9,6 @@ import { ReportService } from 'src/app/services/App/report/report.service';
   styleUrls: ['./report.component.scss'],
 })
 export class ReportComponent implements OnInit {
-
   public trainingChartData: ChartModel = {
     labels: [],
     data: [] as ChartDataModel[],
@@ -20,16 +19,35 @@ export class ReportComponent implements OnInit {
   };
   public barChartOptions: ChartOptions = {
     responsive: true,
-    plugins: { legend: { display: true } }
+    plugins: { legend: { display: true } },
   };
   public barChartType: ChartType = 'bar';
 
-  constructor(public report_service: ReportService) {
+  public trainingPage: number = 0;
+  public foodPage: number = 0;
+  public pageSize: number = 2;
+  Math: any = Math;
 
-  }
+  constructor(public report_service: ReportService) {}
   async ngOnInit(): Promise<void> {
     this.trainingChartData = await this.report_service.getTrainingData();
     this.foodChartData = await this.report_service.getFoodData();
   }
 
+  get pagedTrainingLabels() {
+    const start = this.trainingPage * this.pageSize;
+    return this.trainingChartData.labels.slice(start, start + this.pageSize);
+  }
+  get pagedTrainingData() {
+    const start = this.trainingPage * this.pageSize;
+    return this.trainingChartData.data.slice(start, start + this.pageSize);
+  }
+  get pagedFoodLabels() {
+    const start = this.foodPage * this.pageSize;
+    return this.foodChartData.labels.slice(start, start + this.pageSize);
+  }
+  get pagedFoodData() {
+    const start = this.foodPage * this.pageSize;
+    return this.foodChartData.data.slice(start, start + this.pageSize);
+  }
 }
